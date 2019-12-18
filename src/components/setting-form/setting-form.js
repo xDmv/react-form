2e8a1a06b5f206948aecf0084a0c41a461ea2549
Daemon <makushin.dima@gmail.com>
2019-12-18 18:06:42 +0200

    up: temp version

--- src/components/setting-form/setting-form.js
+++ src/components/setting-form/setting-form.js
@@ -5,15 +5,31 @@ export default class SettingForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			arrform: []
+			arrform: [],
+			isGenerateBtn: false
 		}
 	}
 
+	GenerateForm(array) {
+		console.log('Генерирую форму...')
+	}
+
 	render() {
+		const { isGenerateBtn } = this.state
 		return (
 			<div className="text-center">
 				<h3>Добавьте необходимые элементы для создания формы.</h3>
-				<SettingItem addElement={() => console.log('add element array')}/>
+				<SettingItem addElement={() => console.log('add element array')} />
+				{isGenerateBtn &&
+					(<div className="col-sm-12 col-md-12 text-center">
+						<button
+							type="button"
+							className="btn btn-primary"
+							onClick={this.GenerateForm} >
+								Сгенерировать форму
+						</button>
+					</div>)
+				}
 			</div>
 		);
 	}
